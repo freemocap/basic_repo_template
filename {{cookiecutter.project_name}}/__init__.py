3cb621140781c1e7ee501f769e835b6220a5018e
philipqueen <queen.philip@gmail.com>
2023-02-07 14:42:33 -0700

    Try implementing cookiecutter

--- {{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/__init__.py
@@ -1,12 +1,12 @@
 """Top-level package for basic_template_repo."""
 
-__package_name__ = "basic_repo_template"
+__package_name__ = {{cookiecutter.project_name}}
 __version__ = "v2023.01.1004"
 
-__author__ = """Skelly FreeMoCap"""
-__email__ = "info@freemocap.org"
-__repo_owner_github_user_name__ = "freemocap"
-__repo_url__ = f"https://github.com/{__repo_owner_github_user_name__}/{__package_name__}/"
+__author__ = {{cookiecutter.author}}
+__email__ = {{cookiecutter.email}}
+__repo_owner_github_user_name__ = {{cookiecutter.repo_owner_github_user_name}}
+__repo_url__ = f"https://github.com/{__repo_owner_github_user_name__})/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"
 
 import sys
@@ -20,8 +20,8 @@ base_package_path = Path(__file__).parent
 print(f"adding base_package_path: {base_package_path} : to sys.path")
 sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
 
-from basic_repo_template.system.default_paths import get_log_file_path
-from basic_repo_template.system.logging_configuration import configure_logging
+from {{cookiecutter.project_name}}.system.default_paths import get_log_file_path
+from {{cookiecutter.project_name}}.system.logging_configuration import configure_logging
 
 
 configure_logging(log_file_path=get_log_file_path())
